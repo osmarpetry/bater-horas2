478bfa0bd3d981fa7868eb4a72e11f5af0a2c0d6
osmarpetry <osmarpetry@gmail.com>
2020-11-03 00:51:41 -0300

    crud is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import Axios, { AxiosError } from 'axios';
+import React, { useEffect, useState } from 'react';
+import useSWR, { mutate, trigger } from 'swr';
+import useDebounce from 'useDebouce';
+
+interface PontosData {
+  created_at: Date;
+  edited?: boolean;
+  id: number;
+  note: string;
+  published_at?: Date;
+  updated_at?: Date;
+}
+
+interface NotesProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+function Notes({ value, onChange }: NotesProps) {
+  const [stateValue, setStateValue] = useState(value);
+  const sateValueDebouce = useDebounce(stateValue, 1000);
+
+  useEffect(() => {
+    onChange(sateValueDebouce);
+  }, [onChange, sateValueDebouce]);
+
+  return (
+    <input
+      placeholder="No notes"
+      value={stateValue}
+      onChange={(element) => setStateValue(element.target.value)}
+    />
+  );
+}
 
 function App() {
-  return <div>Bater Horas 2</div>;
+  const { data = [] } = useSWR<PontosData[], AxiosError>(
+    'http://localhost:1337/pontos',
+    (url: string) => Axios.get(url).then((data) => data.data)
+  );
+
+  return (
+    <main>
+      <h1>Bater horas</h1>
+      <button
+        onClick={async () => {
+          const url = 'http://localhost:1337/pontos';
+          console.info('Created at: ', new Date());
+          mutate(
+            url,
+            [
+              ...data,
+              {
+                created_at: new Date().toString(),
+                note: '',
+                id: data.length > 0 ? data[data.length - 1].id + 1 : 1,
+              },
+            ],
+            false
+          );
+          await Axios.post('http://localhost:1337/pontos');
+          trigger(url);
+        }}
+      >
+        Create new point
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <td>Created At</td>
+            <td>Notes</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map(({ created_at, note, id }) => {
+            const handleNoteChange = (value: string) => {
+              Axios.put(`http://localhost:1337/pontos/${id}`, { note: value });
+            };
+
+            return (
+              <tr key={id}>
+                <td>{created_at}</td>
+                <td>
+                  <Notes value={note} onChange={handleNoteChange} />
+                </td>
+                <td>
+                  <button
+                    onClick={async () => {
+                      const url = 'http://localhost:1337/pontos';
+                      console.info('Deleted at: ', id);
+                      mutate(
+                        url,
+                        data.filter((item) => item.id !== id),
+                        false
+                      );
+                      await Axios.delete(`http://localhost:1337/pontos/${id}`);
+                      trigger(url);
+                    }}
+                  >
+                    Apagar
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
 }
 
 export default App;
